4e7f8c9fa1c5729c92a163f995b8056527ca2cb4
Lin Yun <1459440013@qq.com>
2025-05-15 14:45:13 +0800

    fix the problem while tile == null

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -155,6 +155,7 @@ public class Model extends Observable {
         // TODO: Fill in this function.
         for(int i = 0; i < b.size(); i ++) {
             for(int j = 0; j < b.size(); j ++) {
+                if(b.tile(i, j) == null) continue;
                 if(b.tile(i, j).value() == MAX_PIECE) return true;
             }
         }
