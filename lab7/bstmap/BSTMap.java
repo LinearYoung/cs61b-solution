36267e7460752804f1793a9c5323984b32933dad
Lin Yun <1459440013@qq.com>
2025-08-07 10:48:09 +0800

    the lab7 commit

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,203 @@
+package bstmap;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
+    public class BSTNode {
+        private K key;
+        private V value;
+        private BSTNode left;
+        private BSTNode right;
+        private int size;
+
+        BSTNode(K key, V value, int size) {
+            this.key = key;
+            this.value = value;
+            this.left = null;
+            this.right = null;
+            this.size = size;
+        }
+    }
+
+    private BSTNode root;
+
+
+    @Override
+    public void clear() {
+        root = null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return BS(root, key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        BSTNode keynode = BS(root, key);
+        return keynode == null ? null : keynode.value;
+    }
+
+    private BSTNode BS(BSTNode node, K key) {
+        if(node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if(cmp > 0) {
+            return BS(node.right, key);
+        }
+        else if(cmp < 0) {
+            return BS(node.left, key);
+        }
+        else {
+            return node;
+        }
+    }
+
+    @Override
+    public int size() {
+        return root.size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+       put(root, key, value);
+    }
+
+    private void put(BSTNode node, K key, V value) {
+        if(node.size == 0) {
+            root = new BSTNode(key, value, 1);
+        }
+        int cmp = key.compareTo(node.key);
+        if(cmp > 0) {
+            put(node.right, key, value);
+            node.size += 1;
+        } else if (cmp < 0) {
+            put(node.left, key, value);
+            node.size += 1;
+        }
+        else {
+            node.value = value;
+        }
+    }
+
+    @Override
+    public Set<K> keySet() {
+        Set<K> keyset = new HashSet<>();
+        LinkedList<BSTNode> nodequece = new LinkedList<>();
+        nodequece.addLast(root);
+        while (!nodequece.isEmpty()) {
+            BSTNode node = nodequece.removeFirst();
+            if (node == null) continue;
+            keyset.add(node.key);
+            nodequece.addLast(node.left);
+            nodequece.addLast(node.right);
+        }
+        return keyset;
+    }
+
+    @Override
+    public V remove(K key) {
+        if (containsKey(key)) {
+            V val =  get(key);
+            root = remove(root, key);
+            return val;
+        }
+        return null;
+    }
+
+    private BSTNode remove(BSTNode node, K key) {
+        if(node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if(cmp > 0) {
+            remove(node.right, key);
+        }
+        else if (cmp < 0){
+            remove(node.left, key);
+        }
+        else {
+            //有0或1个节点直接将子节点接入该节点的父节点
+            if(node.left == null) {
+                return node.right;
+            }
+            if(node.right == null) {
+                return node.left;
+            }
+            //该节点有两个子节点，寻找successor来代替该节点
+            //并对右侧部分进行删除successor节点操作，因为successor必然只有0或1个节点，从而实现问题转化
+            BSTNode temp = node;
+            node = findMin(node.left);
+            node.right = removeMin(temp.right);
+            node.left = temp.left;
+        }
+        node.size = 1 + node.left.size + node.right.size;
+        return node;
+    }
+
+    BSTNode findMin(BSTNode node) {
+        if(node.left == null) {
+            return node.right;
+        }
+        else return findMin(node.left);
+    }
+
+    BSTNode removeMin(BSTNode node) {
+        if(node.left == null) {
+            return node.right;
+        }
+        node.left = removeMin(node.left);
+        node.size = 1 + node.left.size + node.right.size;
+        return node;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        if(get(key).equals(value)) {
+            remove(root, key);
+            return value;
+        }
+        return null;
+    }
+
+
+    @Override
+    public Iterator<K> iterator() {
+        return new BSTIterator();
+    }
+
+    private class BSTIterator implements Iterator<K> {
+        private Deque<BSTNode> stack = new LinkedList<>();
+
+
+        BSTIterator() {
+            BSTNode current = root;
+            while(current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+        }
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public K next() {
+            if(!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            BSTNode node = stack.pop();
+            K res = node.key;
+            BSTNode right = node.right;
+            while(right != null) {
+                stack.push(right);
+                right = right.left;
+            }
+            return res;
+        }
+    }
+}
